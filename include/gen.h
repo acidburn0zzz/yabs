f80e44e09a5669c6275880b8250346faca1c895a
Alberto Corona <alberto@0x1a.us>
2014-07-15 18:16:41 -0500

    Add FS defines, move includes, add member functions
    	- Defines define flags for WalkDir
    	- Fix includes order to that of:
    		C includes
    		C++ includes
    		Project includes
    		External library includes
    
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- include/gen.h
+++ include/gen.h
@@ -1,14 +1,24 @@
 #ifndef _GEN_H
 #define _GEN_H
-#define MAKEFILE "Makefile"
 
-#include <fstream>
+#define MAKEFILE "Makefile"
+#define FS_NONE 0
+#define FS_RECURSIVE (1 << 0)
+#define FS_DEFAULT FS_RECURSIVE
+#define FS_FOLLOWLINK (1 << 1)
+#define FS_DOTFILES (1 << 2)
+#define FS_MATCHDIRS (1 << 3)
+
+#include <regex.h>
+#include <unistd.h>
 #include <sys/param.h>
+#include <fstream>
 
 class Generate
 {
 public:
 	char cwd[MAXPATHLEN];
+	char *currentDir = get_current_dir_name();
 	const char *defaultMakefile;
 	FILE *Makefile;
 	FILE *newConfig;
@@ -17,9 +27,13 @@ public:
 	~Generate();
 	char *DefineBaseDir();
 	void WriteMake();
+	void Walk();
 	void GenBlankConfig();
 	void CheckFiles();
-	void ListDir(const char *Path);
+	int CheckConfigExists();
 	int CheckMake();
+	int GenMakeFromTemplate();
+	int WalkDir(const char *DirName, char *Pattern, int Spec);
+	int WalkRecur(const char *DirName, regex_t *Expr, int Spec);
 };
 #endif
