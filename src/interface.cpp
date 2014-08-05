62236f392b2002b11fac1704e0e59452cf01c419
Alberto Corona <alberto@0x1a.us>
2014-08-05 18:40:26 -0500

    Fix help format
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- src/interface.cpp
+++ src/interface.cpp
@@ -6,6 +6,6 @@ void printHelp()
 	printf("Usage: yabs [options]\n\n"
 	       "yabs, a simple build system.\n"
 	       "Options:\n"
-	       "-new\tCreate a new yabs build file\n"
-	       "-help\tPrint this dialog\n");
+	       "\t-new\tCreate a new yabs build file\n"
+	       "\t-help\tPrint this dialog\n");
 }
