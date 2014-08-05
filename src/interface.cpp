1456979029170f8a8b4c7f845336b925a6d86154
Alberto Corona <alberto@0x1a.us>
2014-08-05 18:52:49 -0500

    Fix help print to reflect parameter changes
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- src/interface.cpp
+++ src/interface.cpp
@@ -6,6 +6,6 @@ void printHelp()
 	printf("Usage: yabs [options]\n\n"
 	       "yabs, a simple build system.\n"
 	       "Options:\n"
-	       "\t-new\tCreate a new yabs build file\n"
-	       "\t-help\tPrint this dialog\n");
+	       "\t-n\tCreate a new yabs build file\n"
+	       "\t-h\tPrint this dialog\n");
 }
