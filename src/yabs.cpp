4b8542471ee4c7f881f873f5f3061b404726c4c6
Alberto Corona <alberto@0x1a.us>
2014-08-09 21:58:05 -0700

    yabs: add long options
    	- Debug option can now take a configuration file as an argument
    
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- src/yabs.cpp
+++ src/yabs.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <unistd.h>
+#include <getopt.h>
 #include "gen.h"
 #include "interface.h"
+#include "parser.h"
 
 int main(int argc, char *argv[])
 {
 	Generate Gen;
-	int flag = 0;
-	int c;
-	while ((c = getopt(argc, argv, "dhnm")) != -1)
+	Parser Prs;
+	char *ybf;
+	while (1) {
+		static struct option long_options[] = {
+			{ "new", optional_argument, 0, 'n' },
+			{ "help", no_argument, 0, 'h' },
+			{ "debug", no_argument, 0, 'd' },
+			{ 0, 0, 0, 0 }
+		};
+		int option_index = 0;
+		int c = getopt_long(argc, argv, "dhnm", long_options, &option_index);
+		if (c == -1)
+			break;
 		switch (c) {
 		case 'd':
 			Gen.CheckMake();
@@ -16,15 +28,15 @@ int main(int argc, char *argv[])
 			Gen.GenMakeFromTemplate();
 			Gen.WalkDir(Gen.current_dir, ".\\.cpp$", FS_DEFAULT | FS_MATCHDIRS);
 			Gen.WalkDir(Gen.current_dir, ".\\.h$", FS_DEFAULT | FS_MATCHDIRS);
+			Prs.OpenConfig(argv[2]);
 			break;
 		case 'h':
 			printHelp();
 			break;
 		case 'n':
-			Gen.GenBlankConfig();
-			break;
-		case 'm':
+			Gen.GenBlankConfig(0);
 			break;
 		}
+	}
 	return 0;
 }
