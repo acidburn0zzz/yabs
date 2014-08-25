6622da40a2cb9ae19a8e49bc8029074aa49af9af
Alberto Corona <alberto@0x1a.us>
2014-08-25 18:27:01 -0500

    yabs: add -p option
    	- The -p option can be used to verbosely parse a configuration
    	  file
    
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- src/yabs.cpp
+++ src/yabs.cpp
@@ -9,16 +9,18 @@ int main(int argc, char *argv[])
 {
 	Generate Gen;
 	Parser Prs;
-	char *ybf;
+	int n_opt, p_opt;
+	char *n_opt_arg;
 	while (1) {
 		static struct option long_options[] = {
-			{ "new", optional_argument, 0, 'n' },
+			{ "new", optional_argument, &n_opt, 'n' },
 			{ "help", no_argument, 0, 'h' },
 			{ "debug", no_argument, 0, 'd' },
+			{ "parse", optional_argument, &p_opt, 'p' },
 			{ 0, 0, 0, 0 }
 		};
 		int option_index = 0;
-		int c = getopt_long(argc, argv, "dhnm", long_options, &option_index);
+		int c = getopt_long(argc, argv, ":d::p::hn::m", long_options, &option_index);
 		if (c == -1)
 			break;
 		switch (c) {
@@ -36,6 +38,13 @@ int main(int argc, char *argv[])
 			break;
 		case 'n':
 			Gen.GenBlankConfig(0);
+			n_opt_arg = optarg;
+			break;
+		case 'p':
+			if (argv[2] != NULL)
+				Prs.OpenConfig(argv[2]);
+			break;
+		case ':':
 			break;
 		}
 	}
