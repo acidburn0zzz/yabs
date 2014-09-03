6baa179da80d9f8c746f4644150173057bc79bf3
Alberto Corona <alberto@0x1a.us>
2014-09-03 18:33:02 -0500

    platdef: change lin_def to unix_def
    	- This might be completely useless...
    
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- include/platdef.h
+++ include/platdef.h
@@ -28,13 +28,13 @@
 #ifndef _PLATDEF
 #define _PLATDEF
 
-#ifdef __linux__
-int lin_def = 1;
+#ifdef __unix__
+int unix_def = 1;
 int win_def = 0;
 #endif
 
 #ifdef __WIN32__
-int lin_def = 0;
+int unix_def = 0;
 int win_def = 1;
 #endif
 
