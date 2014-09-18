5ca49a733d362692ddec461f45ce7c7f16ceeb61
Alberto Corona <alberto@0x1a.us>
2014-09-17 19:15:51 -0500

    yabs: use Yabs class
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- src/yabs.cpp
+++ src/yabs.cpp
@@ -1,42 +1,39 @@
-/* 
- * Copyright (c) 2013-2014, Alberto Corona <alberto@0x1a.us>
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the copyright holder nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright (c) 2013-2014, Alberto Corona <alberto@0x1a.us>
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the copyright holder nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIA DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 #include <iostream>
 #include <unistd.h>
 #include <getopt.h>
 #include "dist.h"
-#include "gen.h"
 #include "interface.h"
-#include "parser.h"
+#include "yabs.h"
 
 int main(int argc, char *argv[])
 {
-	Generate Gen;
-	Parser Prs;
+	Yabs Ybs;
 	int n_opt, p_opt;
 	char *n_opt_arg;
 	while (1) {
@@ -58,24 +55,25 @@ int main(int argc, char *argv[])
 				extract(argv[2]);
 			break;
 		case 'd':
-			Gen.CheckMake();
-			Gen.WriteMake();
-			Gen.GenMakeFromTemplate();
-			Gen.WalkDir(Gen.current_dir, ".\\.cpp$", FS_DEFAULT | FS_MATCHDIRS);
-			Gen.WalkDir(Gen.current_dir, ".\\.h$", FS_DEFAULT | FS_MATCHDIRS);
+			Ybs.CheckMake();
+			Ybs.WriteMake();
+			Ybs.GenMakeFromTemplate();
+			Ybs.WalkDir(Ybs.GetCurrentDir(), ".\\.cpp$", FS_DEFAULT | FS_MATCHDIRS);
+			Ybs.WalkDir(Ybs.GetCurrentDir(), ".\\.h$", FS_DEFAULT | FS_MATCHDIRS);
+			Ybs.PrintFileList();
 			if (argv[2] != NULL)
-				Prs.OpenConfig(argv[2]);
+				Ybs.OpenConfig(argv[2]);
 			break;
 		case 'h':
 			printHelp();
 			break;
 		case 'n':
-			Gen.GenBlankConfig(0);
+			Ybs.GenBlankConfig(0);
 			n_opt_arg = optarg;
 			break;
 		case 'p':
 			if (argv[2] != NULL)
-				Prs.OpenConfig(argv[2]);
+				Ybs.OpenConfig(argv[2]);
 			break;
 		case ':':
 			break;
