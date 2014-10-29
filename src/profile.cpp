61f0cadde726f95117fbd9a61424f90e4b159008
Alberto Corona <alberto@0x1a.us>
2014-10-28 19:49:43 -0500

    profile: remove verbosity
    	- Remove verbosity in profile when adding to appropriate vectors
    
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- src/profile.cpp
+++ src/profile.cpp
@@ -70,62 +70,47 @@ void Profile::PopValidValue(std::string &k_value, const char *v_value)
 {
 	if (strcasecmp("os", k_value.c_str()) == 0) {
 		OSList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("arch", k_value.c_str()) == 0) {
 		ArchList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("cc", k_value.c_str()) == 0) {
 		CCList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("cxx", k_value.c_str()) == 0) {
 		CXXList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("include", k_value.c_str()) == 0) {
 		ConfIncList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("lang", k_value.c_str()) == 0) {
 		LangList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("dist", k_value.c_str()) == 0) {
 		DistList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("before-script", k_value.c_str()) == 0) {
 		BeforeScriptList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("after-script", k_value.c_str()) == 0) {
 		AfterScriptList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("libs", k_value.c_str()) == 0) {
 		LibsList.push_back(PrependLink(v_value, " -l"));
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("incdir", k_value.c_str()) == 0) {
 		IncDirList.push_back(PrependLink(v_value, " -I"));
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("libdir", k_value.c_str()) == 0) {
 		LibDirList.push_back(PrependLink(v_value, " -L"));
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("target", k_value.c_str()) == 0) {
 		TargetList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("remote", k_value.c_str()) == 0) {
 		RemoteList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 	if (strcasecmp("defines", k_value.c_str()) == 0) {
 		DefinesList.push_back(v_value);
-		printf("Adding value: %s to key: %s\n", v_value, k_value.c_str());
 	}
 }
