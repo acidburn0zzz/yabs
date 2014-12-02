53de8d9bda06c605e559aa217aebe57955c1e1f9
Alberto Corona <alberto@0x1a.us>
2014-12-02 17:50:01 -0600

    initial cleanup
    	- This is the first commit in porting this to Rust
    
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- include/colors.h
+++
@@ -1,8 +0,0 @@
-#ifndef _COLORS_H
-#define _COLORS_H
-
-#define CRM "\x1B[0m"
-#define RED "\x1B[31m"
-#define GRN "\x1B[32m"
-
-#endif
--- src/remote.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2013-2014, Alberto Corona <alberto@0x1a.us>
-// All rights reserved. This file is part of yabs, distributed under the BSD
-// 3-Clause license. For full terms please see the LICENSE file.
-
-#include "gen.h"
-#include "remote.h"
-
-Remote::Remote() {}
-Remote::~Remote() {}
