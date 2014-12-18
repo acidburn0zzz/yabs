ee431a19f9eece9f1879386d707d3cd4227b789a
Alberto Corona <alberto@0x1a.us>
2014-12-17 19:14:01 -0600

    profile: inherit `Generate`, add config options
    	- Removed `libdir` option, added `cflags, cxxflags, lflags,`
    	- Add prototypes for writing Makefiles
    
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- include/profile.h
+++ include/profile.h
@@ -8,43 +8,51 @@
 #include <stdlib.h>
 #include <string>
 #include <vector>
+#include "gen.h"
 
-#define MAX_OPT 15
+#define MAX_OPT 18
 
-class Profile
+class Profile : public Generate
 {
-public:
+private:
+	FILE *inc_conf;
+	FILE *Makefile;
 	std::string os;
 	std::string lang;
 	std::string target;
-	std::vector<std::string> arch;
 	std::string cc;
 	std::string cxx;
 	std::string dist;
 	std::string include;
-	std::vector<std::string> libs;
-	std::vector<std::string> incdir;
-	std::vector<std::string> libdir;
 	std::string remote;
 	std::string defines;
+	std::vector<std::string> arch;
+	std::vector<std::string> cxxflags;
+	std::vector<std::string> libs;
+	std::vector<std::string> incdir;
+	std::vector<std::string> lflags;
 	std::vector<std::string> before;
 	std::vector<std::string> after;
-	FILE *inc_conf;
+	std::vector<std::string> obj;
 	std::string plat;
 	std::string p_arch;
 	std::string STDValues[MAX_OPT] = {
-	    "os",     "arch",   "cc",		 "cxx",		 "include",
-	    "lang",   "dist",   "before-script", "after-script", "libs",
-	    "incdir", "libdir", "target",	"remote",       "defines",
+	    "os",	    "arch",	 "cc",      "cxx",    "cflags",
+	    "cxxflags",      "lflags",       "include", "lang",   "dist",
+	    "before-script", "after-script", "libs",    "incdir", "target",
+	    "remote",	"defines",
 	};
+	std::string temp;
 
 public:
 	Profile();
 	int CompValid(unsigned char *comp_value);
 	int PopLists(unsigned char *list_value);
 	int RegValues(const char *reg_value);
+	int WriteMake(const char *makefile);
 	std::string ConvValue(unsigned char *conv_value);
 	std::string PrependLink(std::string link, std::string pre);
+	std::string GetOS() const;
 	void OpenInclude(std::string file);
 	void ParseKey(std::string key);
 	void PopValidValue(std::string k_value, std::string v_value);
@@ -52,6 +60,8 @@ public:
 	void PrintProfile();
 	void CheckBlankValues();
 	void GetSysInfo();
+	void CheckLang();
+	void BuildObjList();
 };
 
 #endif
