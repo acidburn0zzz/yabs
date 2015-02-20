a970381fe48f4df62c23fc83fd15c791ad53b14a
Alberto Corona <alberto@0x1a.us>
2015-02-20 12:17:03 -0600

    initial profile implementation
    	- Still needs full implementation
    
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-#![feature(env,io,core)]
+// Copyright (c) 2013-2014, Alberto Corona <alberto@0x1a.us>
+// All rights reserved. This file is part of yabs, distributed under the BSD
+// 3-Clause license. For full terms please see the LICENSE file.
+
+#![feature(env,io,core,path)]
 
 mod gen;
+mod profile;
 use std::env;
+use std::default::Default;
 use gen::Gen;
+use profile::Profile;
 
 fn main() {
-    let mut generator: Gen = Gen {file_list: Vec::new()};
+    let mut generator: Gen = Default::default();
     generator.recur_walk(&env::current_dir().unwrap());
-    generator.print_file_list();
+    //generator.print_file_list();
+    let mut profile: Profile = Default::default();
+    profile.from_file(Path::new("yabs.toml"));
 }
---
+++ src/profile.rs
@@ -0,0 +1,38 @@
+// Copyright (c) 2013-2014, Alberto Corona <alberto@0x1a.us>
+// All rights reserved. This file is part of yabs, distributed under the BSD
+// 3-Clause license. For full terms please see the LICENSE file.
+
+extern crate toml;
+
+use std::old_io::File;
+use std::default::Default;
+
+#[derive(Default)]
+pub struct Profile {
+    pub os: String,
+    pub lang: String,
+    pub target: String,
+    pub cc: String,
+    pub cxx: String,
+    pub dist: String,
+    pub include: String,
+    pub remote: String,
+    pub arch: Vec<String>,
+    pub cxxflags: Vec<String>,
+    pub libs: Vec<String>,
+    pub incdir: Vec<String>,
+    pub clean: Vec<String>,
+    pub lflags: Vec<String>,
+    pub before: Vec<String>,
+    pub after: Vec<String>,
+    pub obj: Vec<String>,
+}
+
+impl Profile {
+    pub fn from_file(self, file: Path) {
+        let mut f = File::open(&file);
+        let toml = f.read_to_string().unwrap();
+        let val = toml::Parser::new(&toml.as_slice()).parse().unwrap();
+        println!("{:?}", val);
+    }
+}
