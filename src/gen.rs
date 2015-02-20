4959ec9e7947101861a8cb93bfb6f7fc9a98cc52
Alberto Corona <alberto@0x1a.us>
2015-02-20 12:16:41 -0600

    gen: derive Default
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- src/gen.rs
+++ src/gen.rs
@@ -1,9 +1,14 @@
+// Copyright (c) 2013-2014, Alberto Corona <alberto@0x1a.us>
+// All rights reserved. This file is part of yabs, distributed under the BSD
+// 3-Clause license. For full terms please see the LICENSE file.
+
 extern crate regex;
 
 use std::old_path::GenericPath;
 use std::old_io::fs;
 use std::old_io::fs::{PathExtensions};
 
+#[derive(Default)]
 pub struct Gen {
     pub file_list: Vec<Path>,
 }
