a6d88e57bda32b5ed3413fb3115aac93f409fbfc
Alberto Corona <alberto@0x1a.us>
2015-06-22 11:20:28 -0500

    General cleanup
    Signed-off-by: Alberto Corona <alberto@0x1a.us>

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
-// Copyright (c) 2013-2014, Alberto Corona <alberto@0x1a.us>
+// Copyright (c) 2015, Alberto Corona <alberto@0x1a.us>
 // All rights reserved. This file is part of yabs, distributed under the BSD
 // 3-Clause license. For full terms please see the LICENSE file.
 
-#![feature(std_misc,collections, path_ext, path_relative_from)]
-#![allow(unused_imports)]
+static NAME: &'static str = "yabs";
+static VERS: &'static str = "0.1.0";
 
-mod gen;
-mod profile;
+extern crate pgetopts;
+
+use pgetopts::{Options};
+
 use std::env;
-use std::default::Default;
-use std::path::PathBuf;
-use gen::Gen;
-use profile::Profile;
+
+fn print_usage(opts: Options) {
+    println!("{}", opts.options());
+}
 
 fn main() {
-    let mut gen: Gen = Default::default();
-    let mut prof: Profile = Default::default();
-    let test = PathBuf::from("yabs.toml");
-    gen.is_dot(&test);
-    gen.has_ext(&test, &String::from_str("toml"));
-    gen.walk_dir(env::current_dir().unwrap(), &String::from_str("rs"));
-    gen.print_filelist();
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+
+    opts.optflag("n", "new", "Create a new build profile");
+    opts.optflag("h", "help", "Print help information");
+    opts.optflag("", "version", "Print version information");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m },
+        Err(e) => { panic!("{}", e.to_string()); }
+    };
+
+    if matches.opt_present("h") { print_usage(opts); }
+    else if matches.opt_present("version") { println!("{} {}", NAME, VERS); }
 }
